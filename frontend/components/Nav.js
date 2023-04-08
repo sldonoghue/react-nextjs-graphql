927929aa9a2c9aaeba6681914fb093a3000ef1dc
Sarah Donoghue <sarahdonoghue1@hotmail.com>
2023-04-08 11:35:11 +0100

    create/edit production funct

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -4,14 +4,11 @@ import NavStyles from './styles/NavStyles';
 export default function Nav() {
   return (
     <NavStyles>
-      <Link href="/items">
-        Items
+      <Link href="/products">
+        Products
       </Link>
       <Link href="/sell">
         Sell
-      </Link>
-      <Link href="/signup">
-        Signup
       </Link>
       <Link href="/orders">
         Orders
